0ea95b518d92cb45c31ec1625e35841d60141c1f
NapatWannapako <Napat.wa@kkumail.com>
2023-05-10 11:59:15 +0700

    fontend_update
    Landing page.js
    Landing page.css

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -2,16 +2,63 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navigation from './NavBar.js';
+import './LandingPage.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-function LandingPageApp(){
+
+function LandingPageApp() {
     return (
-        <div>
-            <Navigation />
-            <div className="landingPage">
-                <h1>Welcome to the Landing Page</h1>
-            </div>
+      <div>
+          <Navbar className ="navbar" bg="light" expand="lg">
+            <Navbar.Brand href="#">My Website</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                    <Nav.Link href="#">Home</Nav.Link>
+                    <Nav.Link href="#">About</Nav.Link>
+                    <Nav.Link href="#">Contact</Nav.Link>
+                    </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+        <h1 className="title">
+            Welcome
+        </h1>
+        <div className="Boxuser_password">
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label className="textbox">Email or Phone</Form.Label>
+              <Form.Control className="box_sizing" type="email" />
+            </Form.Group>
+  
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label className="textbox">Password</Form.Label>
+              <Form.Control type="password" className="box_sizing" />
+              <Form.Text className="textforgotpassword">Forgot your password?</Form.Text>
+            </Form.Group>
+  
+            <Button className ="box_signin" variant="primary" type="submit">
+              Sign In
+            </Button>
+            <Button className ="box_sigout" variant="primary" type="submit">
+              Sign In
+            </Button>
+            <Button className ="box_sigout" variant="primary" type="submit">
+              Sign In
+            </Button>
+
+
+
+
+          </Form>
         </div>
-    )
+      </div>
+    );
 }
 
 export default LandingPageApp;
