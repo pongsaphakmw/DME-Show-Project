ee568a97e6741cc47c79592f26f76761d7882aec
NapatWannapako <Napat.wa@kkumail.com>
2023-06-22 21:24:24 +0700

    nav user

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,11 +3,20 @@ import { Navbar, Nav } from 'react-bootstrap';
 import './NavBarLandingPage.css';
 import Button from 'react-bootstrap/Button';
 import logo from './image/DMELOGO.svg';
-import Form from 'react-bootstrap/Form';
+
 import Container from 'react-bootstrap/Container';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import {BsFillPeopleFill} from "react-icons/bs";
+import {BsBell} from "react-icons/bs";
+import {BsFillChatFill} from "react-icons/bs";
 import { getAuth } from 'firebase/auth';
+import {BsFillGridFill} from "react-icons/bs";
+import {BsBriefcase} from "react-icons/bs";
+
+import Form from 'react-bootstrap/Form';
+
+
+
 function Navigation() {
   const [expand, setExpand] = useState('sm');
   const auth = getAuth();
@@ -24,7 +33,7 @@ if (expand){
 }
 return (
   <div> 
-  <Navbar expand={expand} className="mb-3">
+  <Navbar expand={expand}>
     
     <Container fluid>
       <Navbar.Brand href="/"> <img src={logo} alt=''/></Navbar.Brand>
@@ -43,22 +52,44 @@ return (
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3 ">
-                  <Nav.Link href="#action1"> 
-                  <BsFillPeopleFill className='incon_people'/>
-                  </Nav.Link >
-                  
-                  {handleExpand2}
+               
                   {user === null ? (
-                      <div>
-                        <Button  className='button ' variant="outline-dark" href="/sign-in">sign in</Button>
+                      <div className="nav-icons">
+                           <Nav.Link href="/"> 
+                          <BsFillPeopleFill className='incon_people'/>
+                          </Nav.Link >
+                          {handleExpand2}
+                        <Button  className='button' variant="outline-dark" href="/sign-in">sign in</Button>
                         
                         <Button className='button_signup' variant="outline-danger" href="/sign-up">sign up</Button>
                       </div>
                     ) : (
-                      <div>
+                    <div className="nav-icons">
+                           <Form className=" form_align d-flex align-items-center"> 
+                        <Form.Control
+                          type="search"
+                          placeholder="Search"
+                          className="me-2"
+                          aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                      </Form>
+                      <Nav.Link href="/">
+                        <BsFillChatFill className="incon_people" />
+                      </Nav.Link>
+                      <Nav.Link href="/">
+                        <BsBell className="incon_people" />
+                      </Nav.Link>
+                      <Nav.Link href="/">
+                        <BsBriefcase className="incon_people" />
+                      </Nav.Link>
+                      <Nav.Link href="/">
+                        <BsFillGridFill className="incon_people" />
+                      </Nav.Link>
                         <Button className='button ' variant="outline-dark" href="/" onClick={handleSignOut}>sign out</Button>
                       </div>
                     )}
+                    
                   
                 </Nav>
               </Offcanvas.Body>
